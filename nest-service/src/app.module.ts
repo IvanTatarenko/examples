e62757dd364de9510fbc4c5a1ebed5d4a8a8bef8
Vakum <vakummail@gmail.com>
2024-02-01 11:54:46 +0200

    sync

--- nest-service/src/app.module.ts
+++ nest-service/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      cache: true,
+      isGlobal: true,
+      load: [configuration],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
