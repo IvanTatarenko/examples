2643c39a3bbb6ede128b384c74be6a53d26cfb19
Ivan Tatarenko <i.tatarenko@mail.inc>
2025-01-23 10:30:43 +0200

    update env config

--- apps/nestjs/env-config.ts
+++ apps/nestjs/env-config.ts
@@ -1,8 +1,25 @@
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+dotenv.config();
+
+const logger = new Logger('ENV_CONFIG');
+
+function validateEnvVar(name: string, value: string | undefined): string {
+  if (!value) {
+    logger.error(`Змінна середовища ${name} не визначена або порожня.`);
+  }
+  return value;
+}
+
 export const ENV_CONFIG = {
   // database
-  DB_HOST: process.env.POSTGRES_HOST,
-  DB_PORT: parseInt(process.env.POSTGRES_PORT),
-  DB_USERNAME: process.env.POSTGRES_USER,
-  DB_PASSWORD: process.env.POSTGRES_PASSWORD,
-  DB_NAME: process.env.POSTGRES_DB,
+  DB_HOST: validateEnvVar('POSTGRES_HOST', process.env.POSTGRES_HOST),
+  DB_PORT: parseInt(validateEnvVar('POSTGRES_PORT', process.env.POSTGRES_PORT)),
+  DB_USERNAME: validateEnvVar('POSTGRES_USER', process.env.POSTGRES_USER),
+  DB_PASSWORD: validateEnvVar(
+    'POSTGRES_PASSWORD',
+    process.env.POSTGRES_PASSWORD,
+  ),
+  DB_NAME: validateEnvVar('POSTGRES_DB', process.env.POSTGRES_DB),
 };
--- apps/nestjs/src/app.module.ts
+++ apps/nestjs/src/app.module.ts
@@ -5,16 +5,18 @@ import { AppService } from './app.service';
 import { ENV_CONFIG } from 'env-config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: ENV_CONFIG.DB_HOST,
-    port: ENV_CONFIG.DB_PORT,
-    username: ENV_CONFIG.DB_USERNAME,
-    password: ENV_CONFIG.DB_PASSWORD,
-    database: ENV_CONFIG.DB_NAME,
-    entities: [__dirname + '../../**/*.entity{.ts,.js}'],
-    synchronize: true,
-  }),],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: ENV_CONFIG.DB_HOST,
+      port: ENV_CONFIG.DB_PORT,
+      username: ENV_CONFIG.DB_USERNAME,
+      password: ENV_CONFIG.DB_PASSWORD,
+      database: ENV_CONFIG.DB_NAME,
+      entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
